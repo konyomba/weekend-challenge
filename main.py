e5b1b57eff3aaaa68840e7ba1fed281653a43000
konyomba <konyomba@gmail.com>
2025-02-23 19:36:14 +0300

    bugs check

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from flask_wtf import CSRFProtect
 from forms import ObesityPredictionForm, RegistrationForm, LoginForm
 import redis
 import requests
+from flask_mysqldb import MySQL
+
+
 
 app = Flask(__name__)
 
@@ -20,12 +23,24 @@ app.config['SESSION_REDIS'] = redis.Redis(host='localhost', port=6379, db=0)
 Session(app)
 app.config['SECRET_KEY'] = '86cf016117fc3c20024cbece2419d4ce'
 
+
+#DB configurations
+app.config['MYSQL_HOST']='127.0.0.1'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='K3v!n254'
+app.config['MYSQL_DB']='Health'
+
+
+
+mysql=MySQL(app)
+
+
 @app.route('/')
 def main_page():
     return render_template('index.html')
 
 @app.route('/about us')
-def about_us():
+def about():
     pass
 
 @app.route('/contact')
@@ -81,7 +96,7 @@ def login():
     error = None
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data != 'konyomba@gmail.com' or form.password.data != '1234':
+        if form.Email.data != 'konyomba@gmail.com' or form.password.data != '1234':
             error = 'Invalid credentials'
             flash('Invalid credentials', 'danger')
         else:
@@ -90,4 +105,4 @@ def login():
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000,host='192.168.100.4')
